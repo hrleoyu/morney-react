a197bff2ea65f878e7345688a8ec7b5831e0abb1
hrleoyu <623377828@qq.com>
2020-06-05 14:16:08 +0800

    svg 图片

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,10 @@
 import styled from "styled-components";
 import {Link} from "react-router-dom";
 import React from "react";
+require( './icons/money.svg');
+require('./icons/stiat.svg');
+require('./icons/tags.svg');
+
 
 const NavWrapper = styled.nav`
   line-height: 24px;
@@ -10,7 +14,13 @@ const NavWrapper = styled.nav`
     >li {
     width: 33.333333%;
     text-align: center;
-    padding: 16px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+      .iconNav {
+      width: 32px;
+      height: 32px;  
+      }
     }
   }
 `;
@@ -20,12 +30,21 @@ const Nav  = () => {
         <NavWrapper>
             <ul>
                 <li>
+                    <svg className={'iconNav'}>
+                        <use xlinkHref={'#tags'}></use>
+                    </svg>
                     <Link to="/tags">标签</Link>
                 </li>
                 <li>
+                    <svg className={'iconNav'}>
+                        <use xlinkHref="#money"></use>
+                    </svg>
                     <Link to="/money">记账</Link>
                 </li>
                 <li>
+                    <svg className={'iconNav'}>
+                        <use xlinkHref={'#stiat'}></use>
+                    </svg>
                     <Link to="/statistics">统计</Link>
                 </li>
             </ul>
