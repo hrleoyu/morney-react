7573d18e33ea5f1b8539e0d4a50e0763b5cf4730
hrleoyu <623377828@qq.com>
2020-06-10 18:25:08 +0800

    tag List 完成

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -1,7 +1,7 @@
 import {useState} from "react";
 
 
-const useTags = () => {
+const useTags = () => {//封装一个自定义hook
     const [tags,setTags] = useState<string[]>(['服装','吃饭','住宿','交通','运动','购物',]);
     return{
         tags ,
--- src/views/tags.tsx
+++ src/views/tags.tsx
@@ -1,13 +1,50 @@
 import Layout from "../components/Layout";
 import React from "react";
 import {useTags} from "../useTags";
+import styled from "styled-components";
+import Icon from "../components/icon";
 
 function Tags() {
+    const TagList = styled.ol`
+    font-size: 16px;
+      >li{
+      border-bottom: 1px solid #faf3be; 
+      line-height: 20px;    
+      padding: 12px 16px  ;
+      margin-left: 16px;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      }
+`;
+    const ButtonList = styled.button`
+    font-size: 18px;
+    padding: 4px 4px;
+    background:#faf3be;
+    border-radius: 4px;
+    margin-top: 20px;
+`;
+    const Center = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
     const {tags,setTags} = useTags();
     return (
         <Layout>
-            <h2>标签</h2>
-        </Layout>
+            <TagList>
+            {tags.map(tag =>
+                <li key={tag}>
+                    {tag}
+                    <Icon name={'right'}/>
+                </li>
+            )}
+            </TagList>
+            <Center>
+            <ButtonList>新增标签</ButtonList>
+            </Center>
+            </Layout>
     );
 }
 
